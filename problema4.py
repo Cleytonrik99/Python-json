5075c2d1344e76714eb1091e2fbf82644d40a2fb
Cleytonrik99 <enriquecleyton@gmail.com>
2025-03-06 09:43:30 -0300

    exerc4

--- problema4.py
+++ problema4.py
@@ -7,18 +7,43 @@ def menu():
     opcao = int(input("Opcao: "))
     return opcao
 
-def leitura_arquivo():
-    print("Lendo arquivo")
+def leitura_arquivo() -> dict:
+    # print("Lendo arquivo")
+    try:
+        with open("d:/pix.dat", mode="r", coding="utf8") as arq:
+            retorno = json.load(arq)
+            return retorno
+    except Exception:
+        return {}
+
 
 def cadastro(info: dict):
-    print("cadastrando info PIX")
+    # print("cadastrando info PIX")
+    chave = input("Informe chave pix: ")
+    if chave in info:
+        print("Chave existente!")
+    else:
+        nome = input("Nome: ")
+        doc = input("Documento: ")
+        banco = input("Banco: ")
+        conta = input("Conta: ")
+
+        valor = {"nome": nome, "doc": doc, "banco": banco, "conta": conta}
+        
+        info[chave] = valor
 
 def consulta(dados) -> object:
-    print("Fazendo a consulta")
-    return None
+    # print("Fazendo a consulta")
+    chave = input("chave pix: ")
+    if chave in info:
+        return info[chave]
+    else:
+        return "Nenhuma informação encontrada"
 
 def gravacao_arquivo(info: dict):
-    print("Gravando arquivos")
+    # print("Gravando arquivos")
+    with open("d:/pix.dat", mode="w", coding="utf8") as arq:
+        json.dump(info, arq, indent=2)
 
 if __name__ == "__main__":
     print("SISTEMA CHAVE PIX")
